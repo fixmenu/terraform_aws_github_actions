bc4b2603b505a33c23514115f885525a3e3e0bb9
mert gönkesen <mert@yourporter.com>
2023-09-18 17:55:59 +0200

    test

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex h-full flex-col items-center justify-center p-24">
       <div>
       <div className='colorful-divider w-full'></div>
-      <h1 className='codeslag-text'>Codeslag</h1>
+      <h1 className='codeslag-text'>Test</h1>
         <div className='colorful-divider w-full'></div>
       </div>
         
