c22c7ee85c71ce36a324d3b3c645a3f2d50661bc
mert gönkesen <mert@yourporter.com>
2023-09-18 14:08:15 +0200

    style

--- my-blog/src/app/page.tsx
+++ my-blog/src/app/page.tsx
@@ -2,8 +2,13 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex h-full items-center justify-center p-24">
-        <h1 className='codeslag-text'>Codeslag</h1>
+    <main className="flex h-full flex-col items-center justify-center p-24">
+      <div>
+      <div className='colorful-divider w-full'></div>
+      <h1 className='codeslag-text'>Codeslag</h1>
+        <div className='colorful-divider w-full'></div>
+      </div>
+        
     </main>
   )
 }
