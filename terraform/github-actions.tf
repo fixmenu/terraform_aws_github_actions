e96d5d6e9c17dc74a33cdaf430e464fd1cadafbe
mert gönkesen <mert@yourporter.com>
2023-09-18 12:48:08 +0200

    get ec2 host from terraform

--- terraform/github-actions.tf
+++ terraform/github-actions.tf
@@ -13,3 +13,19 @@ resource "github_actions_environment_secret" "ec2-ssh-key" {
   plaintext_value = file("my-keypair")
   repository = data.github_repository.repo.name
 }
+
+
+resource "github_actions_environment_secret" "ec2-host" {
+  environment = github_repository_environment.test_env.environment
+  secret_name = "aws_ec2_host"
+  plaintext_value = aws_instance.terraform-test-server.public_ip
+  repository = data.github_repository.repo.name
+}
+
+
+resource "github_actions_environment_secret" "ec2-user" {
+  environment = github_repository_environment.test_env.environment
+  secret_name = "aws_ec2_user"
+  plaintext_value = "ec2-user"
+  repository = data.github_repository.repo.name
+}
